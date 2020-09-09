5070919e7607449ff84c562573a24aa50ba9257a
Liang Wu <liangwuweb@gmail.com>
2020-09-08 21:26:27 -0500

    Youtube embed video & modal

--- js/script.js
+++ js/script.js
@@ -2,5 +2,21 @@ $(document).ready(function () {
     $('.carousel').carousel({
         interval: 5000,
         pause: false
+    }) 
+    
+    //Modal
+    $('#universeModal').on('shown.bs.modal', function (event) {
+        var card = $(event.relatedTarget); // Card that triggered the modal
+        var videoSrc = card.data('src'); // Extract info from data-* attributes
+
+        // Update the modal's content, and set it to autoplay
+        var modal = $(this);
+        modal.find('#video').attr('src', videoSrc + '?&autoplay=1');
+
+        //Stop the video when hide the modal
+        $("#universeModal").on("hide.bs.modal", function (event) {
+            var modal = $(this);
+            modal.find('#video').attr('src', '');
+        });
     })
 });
